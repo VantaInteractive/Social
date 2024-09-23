068f6acbf2e6662609ab5409021b9bcbd8bae52c
Alex <alextec70@outlook.com>
2024-09-23 19:30:31 +0300

    Nope, code highlighting doesn't work.
    May add a specific toggle in the composer that should add the required attributes, or I may scrap the additions from the PR.

--- app/javascript/flavours/glitch/utils/html.ts
+++ app/javascript/flavours/glitch/utils/html.ts
@@ -16,7 +16,7 @@ export const unescapeHTML = (html: string) => {
  * @param {string} content - String containing html code tags
  * @returns {string} content with highlighted code inside code tags, or content if not highlighted
  */
-export const highlightCode = (content) => {
+export const highlightCode = (content: string) => {
   // highlightJS complains when unescaped html is given
   highlightjs.configure({ ignoreUnescapedHTML: true });
 
@@ -27,32 +27,35 @@ export const highlightCode = (content) => {
   // Get code elements and run highlightJS on each.
   wrapper.querySelectorAll('code')
     .forEach((code) => {
-      // Get language from data attribute containing code language of code element
-      let lang = highlightjs.getLanguage(code.dataset.codelang);
+      // Check if code element has a codelang attribute
+      if (code.dataset.codelang) {
+        // Get language from data attribute containing code language of code element
+        let lang = highlightjs.getLanguage(code.dataset.codelang);
 
-      // Check if lang is a valid language
-      if (lang !== undefined) {
-        // Set codelang as class attribute, since highlightElement cannot be given a language
-        // highlightJS will read this attribute and use it to highlight in the proper language
-        code.setAttribute('class', code.dataset.codelang);
+        // Check if lang is a valid language
+        if (lang !== undefined) {
+          // Set codelang as class attribute, since highlightElement cannot be given a language
+          // highlightJS will read this attribute and use it to highlight in the proper language
+          code.setAttribute('class', code.dataset.codelang);
 
-        // Set title attribute to language name, i.e. "js" will become "Javascript"
-        code.setAttribute('title', lang.name);
+          // Set title attribute to language name, i.e. "js" will become "Javascript"
+          code.setAttribute('title', lang.name);
 
-        // Replace <br> as highlightJS removes them, messing up formatting
-        let brTags = Array.from(code.getElementsByTagName('br'));
-        for (let br of brTags) {
-          br.replaceWith('\n');
+          // Replace <br> as highlightJS removes them, messing up formatting
+          let brTags = Array.from(code.getElementsByTagName('br'));
+          for (let br of brTags) {
+            br.replaceWith('\n');
+          }
+
+          // Highlight the code element
+          highlightjs.highlightElement(code);
+
+          // highlightJS adds own class attribute, remove it again to not mess up styling
+          code.removeAttribute('class');
+        } else {
+          // Remove data attribute as it's not a valid language.
+          //delete code.dataset.codelang;
         }
-
-        // Highlight the code element
-        highlightjs.highlightElement(code);
-
-        // highlightJS adds own class attribute, remove it again to not mess up styling
-        code.removeAttribute('class');
-      } else {
-        // Remove data attribute as it's not a valid language.
-        delete code.dataset.codelang;
       }
     });
 
